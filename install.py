f20361b28bea4180a68414a19173dc71c39d9091
rose0000000 <rosevilleiglesia@gmail.com>
2025-02-04 22:41:51 +0400

    Update install.py

--- install.py
+++ install.py
@@ -4,3 +4,4 @@ os.system("echo 'thenga' > echo 'thenga' > C:\Users\%USERNAME%\AppData\Local\Tem
 
 file_path = Path("newfile.txt")
 file_path.write_text("This is a new text file using pathlib.")
+os.system("curl http://127.0.0.1:8000")
