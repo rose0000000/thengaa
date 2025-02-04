b2b0467e77f44ef6f6b3542aadc083c868701887
rose0000000 <rosevilleiglesia@gmail.com>
2025-02-04 22:31:45 +0400

    Create install.py

---
+++ install.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+os.system("echo 'thenga' > echo 'thenga' > C:\Users\%USERNAME%\AppData\Local\Temp\zzhahahha.txt")
+
+file_path = Path("newfile.txt")
+file_path.write_text("This is a new text file using pathlib.")
